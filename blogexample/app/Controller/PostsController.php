2fc6dbdd930caa3002f227ac4feeecb9a5fa66ae
ehermza <ehermza@yahoo.com>
2021-04-27 00:46:35 -0300

    bueno

---
+++ blogexample/app/Controller/PostsController.php
@@ -0,0 +1,36 @@
+<?php
+class PostsController extends AppController {
+    public $name = 'Posts';
+    public $components = array('Session');
+
+    public function index() {
+        $this->set('posts', $this->Post->find('all'));
+    }
+
+    public function view($id) {
+        $this->Post->id = $id;
+        $this->set('post', $this->Post->read());
+
+    }
+
+    public function add() {
+        if ($this->request->is('post')) {
+            if ($this->Post->save($this->request->data)) {
+                $this->Session->setFlash('Your post has been saved.');
+                $this->redirect(array('action' => 'index'));
+            }
+        }
+    }
+	function edit($id = null) {
+		$this->Post->id = $id;
+		if ($this->request->is('get')) {
+			$this->request->data = $this->Post->read();
+		} else {
+			if ($this->Post->save($this->request->data)) {
+				$this->Session->setFlash('Your post has been updated.');
+				$this->redirect(array('action' => 'index'));
+			}
+		}
+	}	
+}
+?>
---
+++ blogexample/app/View/Posts/delete.ctp
@@ -0,0 +1,10 @@
+<?php
+function delete($id) {
+    if (!$this->request->is('post')) {
+        throw new MethodNotAllowedException();
+    }
+    if ($this->Post->delete($id)) {
+        $this->Session->setFlash('The post with id: ' . $id . ' has been deleted.');
+        $this->redirect(array('action' => 'index'));
+    }
+}
